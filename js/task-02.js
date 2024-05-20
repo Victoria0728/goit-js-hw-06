ab74d25116cfece2a1e9ff926a2cb8ef2223715c
Victoria0728 <vickyvillamor0728@gmail.com>
2024-05-20 22:57:44 +0800

    update task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,13 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 const emptyUL = document.getElementById('ingredients');
-// console.log(emptyUL);
-// ingredients.forEach(ingredients => {
-//     console.log(ingredients);
-//     const liofEmptyUL = document.createElement('li');
-//     liofEmptyUL.textContent = ingredients;
-//     liofEmptyUL.classList.add('item');
-//     console.log(liofEmptyUL);
-//     emptyUL.appendChild(liofEmptyUL);
+console.log(emptyUL);
+ingredients.forEach(ingredients => {
+    console.log(ingredients);
+    const liofEmptyUL = document.createElement('li');
+    liofEmptyUL.textContent = ingredients;
+    liofEmptyUL.classList.add('item');
+    console.log(liofEmptyUL);
+    emptyUL.appendChild(liofEmptyUL);
 
-// });
+});
