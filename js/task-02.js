52f584cc97ec300c6f52f99bcf878c27e51036d0
Victoria0728 <vickyvillamor0728@gmail.com>
2024-05-20 22:52:39 +0800

    add task

---
+++ js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const emptyUL = document.getElementById('ingredients');
+// console.log(emptyUL);
+// ingredients.forEach(ingredients => {
+//     console.log(ingredients);
+//     const liofEmptyUL = document.createElement('li');
+//     liofEmptyUL.textContent = ingredients;
+//     liofEmptyUL.classList.add('item');
+//     console.log(liofEmptyUL);
+//     emptyUL.appendChild(liofEmptyUL);
+
+// });
