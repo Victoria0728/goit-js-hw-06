68578464a1a72203b18dfcb039fb5ee24ba4c3a3
Victoria0728 <vickyvillamor0728@gmail.com>
2024-05-21 12:03:32 +0800

    update 7 8 9 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
-
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', function(event) {
+event.preventDefault();
+const emailInput = loginForm.elements.email;
+const passwordInput = loginForm.elements.password;
+if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+    alert('All fields must be filled in.');
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value
+    };
+    console.log(formData);
+    loginForm.reset();
+  }
+});
